b3f989b1e54daebfd09bcc63ebf8bfbb79b5123e
Taki Monroe <taki.monroeee@gmail.com>
2024-12-29 01:32:11 +0600

    Update nglx.py

--- nglx.py
+++ nglx.py
@@ -1,6 +1,10 @@
-import json, random, time, httpx, sys, os
-from concurrent.futures import ThreadPoolExecutor
-import threading
+import json
+import random
+import time
+import aiohttp
+import asyncio
+import sys
+import os
 from colorama import Fore, Style
 from time import strftime, localtime
 
@@ -13,22 +17,21 @@ with open("config.json") as config:
 class Console:
    @staticmethod
    def Logger(content: str, status: bool) -> None:
-     lock = threading.Lock()
      green = "[" + Fore.GREEN + Style.BRIGHT + "+" + Style.RESET_ALL + "] "
      red = "[" + Fore.RED + Style.BRIGHT + "-" + Style.RESET_ALL + "] "
      yellow = "[" + Fore.YELLOW + Style.BRIGHT + "!" + Style.RESET_ALL + "] "
-     with lock:
-       if status == "g":
+     if status == "g":
          sys.stdout.write(f'{green}{content}\n')
-       elif status == "r":
+     elif status == "r":
          sys.stdout.write(f'{red}{content}\n')
-       elif status == "y":
+     elif status == "y":
          sys.stdout.write(f'{yellow}{content}\n')
+
    @staticmethod
    def clear() -> None:
      os.system("cls" if os.name == "nt" else "clear")
 
-def main(username, message, deviceid):
+async def main(username, message, deviceid):
    global errored, sent
    headers = {
       "Content-Type": "application/x-www-form-urlencoded",
@@ -36,29 +39,29 @@ def main(username, message, deviceid):
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
     }
-   Client = httpx.Client(headers=headers)
-   try:
-     postresp = Client.post(f"https://ngl.link/api/submit",data={"username": username,"question": message,"deviceId": deviceid})
-     if postresp.status_code == 200:
-       sent += 1
-       Console.Logger(f"Sent: {sent} | Errored: {errored}",status="g")
-     elif postresp.status_code == 404:
-       Console.Logger(f"User {username} does not exist", status="r")
-       exit()
-     elif postresp.status_code == 429:
-       Console.Logger(f"User {username} is rate limited", status="r")
-       time.sleep(10)
-     else:
-       Console.Logger(f"{postresp.text} {postresp.status_code}", status="r")
-   except Exception as e:
-     errored += 1
-     print(str(e))
-     Console.Logger(f"Error: {e}", status="y")
+   async with aiohttp.ClientSession(headers=headers) as session:
+     try:
+       async with session.post(f"https://ngl.link/api/submit",data={"username": username,"question": message,"deviceId": deviceid}) as resp:
+         if resp.status == 200:
+           sent += 1
+           Console.Logger(f"Sent: {sent} | Errored: {errored}", status="g")
+         elif resp.status == 404:
+           Console.Logger(f"User {username} does not exist", status="r")
+           exit()
+         elif resp.status == 429:
+           Console.Logger(f"User {username} is rate limited", status="r")
+           await asyncio.sleep(10)
+         else:
+           Console.Logger(f"{await resp.text()} {resp.status}", status="r")
+     except Exception as e:
+       errored += 1
+       print(str(e))
+       Console.Logger(f"Error: {e}", status="y")
 
 def deviceid():
    return "".join(random.choice("0123456789abcdefghijklmnopqrstuvwxyz") for i in range(36))
 
-def Main():
+async def Main():
    Console.clear()
    print(Fore.LIGHTRED_EX+ f"""
  __    __   ______   __                 
@@ -93,11 +96,12 @@ $$/   $$/  $$$$$$/  $$$$$$$$/ $$/   $$/
     if message:
        break
     Console.Logger(f"Message cannot be empty", status="y")
-   with ThreadPoolExecutor(max_workers=messagecount) as executor:
-     for x in range(messagecount):
-       executor.submit(main, username, message, deviceid())
-       time.sleep(delay)
+   tasks = []
+   for x in range(messagecount):
+     tasks.append(main(username, message, deviceid()))
+     await asyncio.sleep(delay)
+   await asyncio.gather(*tasks)
    Console.Logger(f"Sent {sent} messages to {username}", status="g")
 
 if __name__ == "__main__":
-   Main()
+   asyncio.run(Main())
